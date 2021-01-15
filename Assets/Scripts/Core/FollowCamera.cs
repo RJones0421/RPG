2d20f2e0f3caf2462ad5b00e0f4101385ec63c62
RJones0421 <rjcj0421@gmail.com>
2021-01-14 22:15:49 -0500

    Added Basic Movement
    Added a click to move system for the character to move with. The character has a model and is animated.

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
